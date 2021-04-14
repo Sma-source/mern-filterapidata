d766c6811d8b624678af23a34f2f2f0fb03229c5
Sma-source <rabhi.smain@gmail.com>
2021-04-14 19:13:43 +0200

    import asyncHandler

--- backend/controllers/bootcampControllers.js
+++ backend/controllers/bootcampControllers.js
@@ -1,17 +1,23 @@
 const Bootcamp = require("../models/Bootcamp");
+const asyncHandler = require("../middleware/asyncHandler");
 
-exports.getAllBootcamps = (req, res, next) => {
-  res.send("Get all bootcamps route");
-};
+exports.getAllBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
 
-exports.createNewBootcamp = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: bootcamps,
+  });
+});
+
+exports.createNewBootcamp = asyncHandler(async (req, res, next) => {
   res.send("Create new bootcamp route");
-};
+});
 
-exports.updateBootcampById = (req, res, next) => {
+exports.updateBootcampById = asyncHandler(async (req, res, next) => {
   res.send("Update a bootcamp by id route");
-};
+});
 
-exports.deleteBootcampById = (req, res, next) => {
+exports.deleteBootcampById = asyncHandler(async (req, res, next) => {
   res.send("Delete a bootcamp by id route");
-};
+});
