4ce46df571f6eff8333e6160d58fa97d86ef5711
Sma-source <rabhi.smain@gmail.com>
2021-04-14 16:17:14 +0200

    add crud routes and modify .env

--- backend/routes/bootcampRoutes.js
+++ backend/routes/bootcampRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-router.route("/");
+// @route - /api/v1/bootcamps/
+router.route("/").get().post();
+
+// @route - /api/v1/bootcamps/id
+router.route("/:id").put().delete();
 
 module.exports = router;
