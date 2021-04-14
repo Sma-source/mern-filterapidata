5718af1b366b8952136b263c7a28263adb0e774c
Sma-source <rabhi.smain@gmail.com>
2021-04-14 16:35:01 +0200

    create crud controllers

---
+++ backend/controllers/bootcampControllers.js
@@ -0,0 +1,15 @@
+exports.getAllBootcamps = (req, res, next) => {
+  res.send("Get all bootcamps route");
+};
+
+exports.getAllBootcamps = (req, res, next) => {
+  res.send("Create new bootcamp route");
+};
+
+exports.updateBootcampById = (req, res, next) => {
+  res.send("Update a bootcamp by id route");
+};
+
+exports.deleteBootcampById = (req, res, next) => {
+  res.send("Delete a bootcamp by id route");
+};
