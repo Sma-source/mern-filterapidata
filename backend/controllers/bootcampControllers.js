449972b5ceb90a4e5e96020ef41877a66c591fa1
Sma-source <rabhi.smain@gmail.com>
2021-04-14 19:24:52 +0200

    add update controller

--- backend/controllers/bootcampControllers.js
+++ backend/controllers/bootcampControllers.js
@@ -1,5 +1,6 @@
 const Bootcamp = require("../models/Bootcamp");
 const asyncHandler = require("../middleware/asyncHandler");
+const ErrorResponse = require("../utils/errorResponse");
 
 exports.getAllBootcamps = asyncHandler(async (req, res, next) => {
   const bootcamps = await Bootcamp.find();
@@ -20,7 +21,23 @@ exports.createNewBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 exports.updateBootcampById = asyncHandler(async (req, res, next) => {
-  res.send("Update a bootcamp by id route");
+  let bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp with id ${req.params.id} was not found`, 404)
+    );
+  }
+
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
 });
 
 exports.deleteBootcampById = asyncHandler(async (req, res, next) => {
