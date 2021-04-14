5f00217bbd54fa228a27cb289005a9f4810ed9ff
Sma-source <rabhi.smain@gmail.com>
2021-04-14 18:19:23 +0200

    create a validation schema

--- backend/models/Bootcamp.js
+++ backend/models/Bootcamp.js
@@ -1,3 +1,21 @@
 const mongoose = require("mongoose");
 
-const bootcampSchema = new mongoose.Schema();
+const bootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provid a name to the bootcamp"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    required: [true.valueOf, "Please provid a rating"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please provid a description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please provid a price"],
+  },
+});
