82380e5e40c087557460256b9dc7965a6ce4b757
Sma-source <rabhi.smain@gmail.com>
2021-04-17 21:12:01 +0200

    create useEffect

--- frontend/src/pages/BootcampsPage.js
+++ frontend/src/pages/BootcampsPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import { Container } from "@material-ui/core";
 import axios from "axios";
 
@@ -6,6 +6,28 @@ const BootcampsPage = () => {
   // Component state
   const [bootcamps, setBootcamps] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  // Side Effects
+  useEffect(() => {
+    let cancel;
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data } = await axios({
+          method: "GET",
+          url: `/api/v1/bootcamps`,
+          cancelToken: new axios.CancelToken((c) => (cancel = c)),
+        });
+
+        setBootcamps(data.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <Container>
       {/* Filtering and Sorting Section */}
