927767cc00084cf22d1e565926f46cbd650c47b2
Sma-source <rabhi.smain@gmail.com>
2021-04-17 19:52:39 +0200

    modify navabar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import { AppBar, ToolBar, Typography } from "@material-ui/core";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <AppBar>
+      <ToolBar>
+        <Typography variant="h5">Bootcamps</Typography>
+      </ToolBar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
