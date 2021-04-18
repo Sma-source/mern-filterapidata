0c36e9a358b1fb8204df96a5d583d80d3472714f
Sma-source <rabhi.smain@gmail.com>
2021-04-18 18:23:51 +0200

    update style : add header card

--- frontend/src/components/BootcampCard.js
+++ frontend/src/components/BootcampCard.js
@@ -1,10 +1,14 @@
+import { Avatar, CardHeader, Typography } from "@material-ui/core";
 import React from "react";
-import BootcampsPage from "../pages/BootcampsPage";
 
-const BootcampCard = () => {
+const BootcampCard = ({ bootcamp }) => {
   return (
     <div>
       <p>{bootcamp.name} </p>
+      <CardHeader
+        avatar={<Avatar />}
+        title={<Typography variant="h6">{bootcamp.name}</Typography>}
+      />
     </div>
   );
 };
