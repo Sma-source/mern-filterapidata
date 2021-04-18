539b55fbc3ced705308afdcb803ed60bd21ac132
Sma-source <rabhi.smain@gmail.com>
2021-04-18 17:39:01 +0200

    modify Bootcamps page: add loader and grid Bootcamp card

--- frontend/src/pages/BootcampsPage.js
+++ frontend/src/pages/BootcampsPage.js
@@ -1,8 +1,27 @@
 import { React, useState, useEffect } from "react";
-import { Container } from "@material-ui/core";
+import {
+  CircularProgress,
+  Container,
+  Grid,
+  makeStyles,
+} from "@material-ui/core";
 import axios from "axios";
 
+const useStyles = makeStyles({
+  root: {
+    marginTop: 20,
+  },
+  loader: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
 const BootcampsPage = () => {
+  // Material UI Styles
+  const classes = useStyles();
   // Component state
   const [bootcamps, setBootcamps] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -29,10 +48,23 @@ const BootcampsPage = () => {
     fetchData();
   }, []);
   return (
-    <Container>
+    <Container className={classes.root}>
       {/* Filtering and Sorting Section */}
 
       {/* Bootcamps Listing */}
+      <Grid container spacing={2}>
+        {loading ? (
+          <div className={classes.loader}>
+            <CircularProgress size="3rem" thickness={5} />
+          </div>
+        ) : (
+          bootcamps.map((bootcamp) => {
+            <Grid item key={bootcamp._id} xs={12} sm={6} md={4} lg={3}>
+              <BootcampCard bootcamp={bootcamp} />
+            </Grid>;
+          })
+        )}
+      </Grid>
     </Container>
   );
 };
