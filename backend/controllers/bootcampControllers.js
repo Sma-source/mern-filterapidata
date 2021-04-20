14a5266542f9a37da20670ea4f4d9363c2e488c0
Sma-source <rabhi.smain@gmail.com>
2021-04-20 19:36:51 +0200

    return ui values api

--- backend/controllers/bootcampControllers.js
+++ backend/controllers/bootcampControllers.js
@@ -5,11 +5,24 @@ const ErrorResponse = require("../utils/errorResponse");
 exports.getAllBootcamps = asyncHandler(async (req, res, next) => {
   let query;
 
+  let uiValues = {
+    filtering: {},
+    sorting: {},
+  };
+
   const reqQuery = { ...req.query };
 
   const removeFields = ["sort"];
 
   removeFields.forEach((val) => delete reqQuery[val]);
+
+  const filterKeys = Object.keys(reqQuery);
+
+  const filterValues = Object.values(reqQuery);
+
+  filterKeys.forEach(
+    (val, idx) => (uiValues.filtering[val] = filterValues[idx])
+  );
 
   let queryStr = JSON.stringify(reqQuery);
 
@@ -23,6 +36,18 @@ exports.getAllBootcamps = asyncHandler(async (req, res, next) => {
   if (req.query.sort) {
     const sortByArr = req.query.sort.split(",");
 
+    sortByArr.forEach((val) => {
+      let order;
+
+      if (val[0] === "-") {
+        order = "descending";
+      } else {
+        order = "ascending";
+      }
+
+      uiValues.sorting[value.replace("-", "")] = order;
+    });
+
     const sortByStr = sortByArr.join(" ");
 
     query = query.sort(sortByStr);
@@ -32,9 +57,21 @@ exports.getAllBootcamps = asyncHandler(async (req, res, next) => {
 
   const bootcamps = await query;
 
+  const maxPrice = await Bootcamp.find().sort(
+    { price: -1 }.limit(1).select("-_id price")
+  );
+
+  const minPrice = await Bootcamp.find().sort(
+    { price: 1 }.limit(1).select("-_id price")
+  );
+
+  uiValues.maxPrice = maxPrice[0].price;
+  uiValues.minPrice = minPrice[0].price;
+
   res.status(200).json({
     success: true,
     data: bootcamps,
+    uiValues,
   });
 });
 
